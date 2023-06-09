6eebf1d9343b08ac6032862885663f51c5a780e7
iHamza7 <khanazam3324@gmail.com>
2023-06-09 10:11:10 +0500

    changed

--- lib/features/auth/controllers/auth_controller.dart
+++ lib/features/auth/controllers/auth_controller.dart
@@ -1,4 +1,10 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import '../repository/auth_repository.dart';
+
+final authControllerProvider = Provider((ref) => AuthController(
+      authRepository: ref.read(authRepositoryProvider),
+    ));
 
 class AuthController {
   final AuthRepository _authRepository;
