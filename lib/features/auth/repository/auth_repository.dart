56b5cb760ba577c89ce8b3e1e37705ec8db36fd1
iHamza7 <khanazam3324@gmail.com>
2023-06-09 09:35:57 +0500

    changed

--- lib/features/auth/repository/auth_repository.dart
+++ lib/features/auth/repository/auth_repository.dart
@@ -1,18 +1,28 @@
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 
-// import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class AuthRepository {
-  final Firebase _firebase;
+  final FirebaseFirestore _firebaseFirestore;
   final FirebaseAuth _firebaseAuth;
   final GoogleSignIn _googleSignIn;
 
   AuthRepository({
-    required Firebase firebase,
+    required FirebaseFirestore firebaseFirestore,
     required FirebaseAuth firebaseAuth,
     required GoogleSignIn googleSignIn,
-  })  : _firebase = firebase,
+  })  : _firebaseFirestore = firebaseFirestore,
         _firebaseAuth = firebaseAuth,
         _googleSignIn = googleSignIn;
+
+  void signInWithGoogle() async {
+    final GoogleSignInAccount? googleSignInAccount =
+        await _googleSignIn.signIn();
+    final googleAuth = await googleSignInAccount?.authentication;
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+  }
 }
