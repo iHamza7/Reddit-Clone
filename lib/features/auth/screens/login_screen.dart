7be72d909d44b1fd8e5feddeef291e841c145da5
iHamza7 <khanazam3324@gmail.com>
2023-06-11 20:28:08 +0500

    lib changed

--- lib/features/auth/screens/login_screen.dart
+++ lib/features/auth/screens/login_screen.dart
@@ -23,7 +23,7 @@ class LoginScreen extends ConsumerWidget {
           TextButton(
             onPressed: () {},
             child: const Text(
-              'Guest',
+              'Guest User',
               style: TextStyle(
                 fontWeight: FontWeight.bold,
                 fontSize: 16,
