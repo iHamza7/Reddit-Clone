c0385f203e3d60bbaeefee5f6a95468d041c2213
iHamza7 <khanazam3324@gmail.com>
2023-06-11 12:56:16 +0500

    lib changed

--- lib/features/auth/controllers/auth_controller.dart
+++ lib/features/auth/controllers/auth_controller.dart
@@ -20,7 +20,9 @@ class AuthController extends StateNotifier<bool> {
         super(false);
 
   void signInWithGoogle(BuildContext context) async {
+    state = true;
     final user = await _authRepository.signInWithGoogle();
+    state = false;
     user.fold((l) => showSnackBar(context, l.message),
         (r) => _ref.read(userProvider.notifier).update((state) => r));
   }
