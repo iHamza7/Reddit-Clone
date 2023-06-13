031c17ed63b02ed968b1589d3d4979608b883ff4
iHamza7 <khanazam3324@gmail.com>
2023-06-13 22:15:58 +0500

    lib changed

--- lib/routes.dart
+++ lib/routes.dart
@@ -2,7 +2,12 @@ import 'package:routemaster/routemaster.dart';
 import 'package:flutter/material.dart';
 
 import 'features/auth/screens/login_screen.dart';
+import 'features/home/screens/home_screen.dart';
 
 final loggedOutRoute = RouteMap(routes: {
   '/': (_) => const MaterialPage(child: LoginScreen()),
+});
+
+final loggedInRoute = RouteMap(routes: {
+  '/': (_) => const MaterialPage(child: HomeScreen()),
 });
