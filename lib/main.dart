7151614072ecfb66f03c63375a2264d922896b1e
iHamza7 <khanazam3324@gmail.com>
2023-06-14 11:40:52 +0500

    lib changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:routemaster/routemaster.dart';
 
+import 'core/common/error.dart';
+import 'core/common/loader.dart';
+import 'features/auth/controllers/auth_controller.dart';
+import 'models/user_models.dart';
 import 'routes.dart';
 import 'theme/pallete.dart';
 import 'firebase_options.dart';
@@ -15,19 +20,47 @@ void main() async {
   runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerStatefulWidget {
   const MyApp({super.key});
 
   @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends ConsumerState<MyApp> {
+  UserModel? userModel;
+
+  void getData(WidgetRef ref, User data) async {
+    userModel = await ref
+        .watch(authControllerProvider.notifier)
+        .getUserData(data.uid)
+        .first;
+    ref.read(userProvider.notifier).update((state) => userModel);
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      title: 'Reddit Clone',
-      // theme: ThemeData(
-      theme: Pallete.darkModeAppTheme,
-      routerDelegate:
-          RoutemasterDelegate(routesBuilder: (context) => loggedOutRoute),
-      routeInformationParser: const RoutemasterParser(),
-    );
+    return ref.watch(authStateChangeProvider).when(
+        data: (data) => MaterialApp.router(
+              debugShowCheckedModeBanner: false,
+              title: 'Reddit Clone',
+              // theme: ThemeData(
+              theme: Pallete.darkModeAppTheme,
+              routerDelegate: RoutemasterDelegate(
+                routesBuilder: (context) {
+                  if (data != null) {
+                    getData(ref, data);
+                    if (userModel != null) {
+                      return loggedInRoute;
+                    }
+                  }
+                  return loggedOutRoute;
+                },
+              ),
+              routeInformationParser: const RoutemasterParser(),
+            ),
+        error: (error, StackTrace) => ErrorText(text: error.toString()),
+        loading: () => const Loader());
   }
 }
