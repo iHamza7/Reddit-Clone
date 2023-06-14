20e26c354adcd3529f30a9e22c66969195cb8207
iHamza7 <khanazam3324@gmail.com>
2023-06-14 15:16:41 +0500

    lib changed

---
+++ lib/features/home/drawers/community_list_drawer.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class CommunityList extends ConsumerWidget {
+  const CommunityList({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container();
+  }
+}
