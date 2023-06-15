b1929c9a6de25627981e08cdc55379fac4b12e53
iHamza7 <khanazam3324@gmail.com>
2023-06-15 12:44:28 +0500

    lib changed

--- lib/main.dart
+++ lib/main.dart
@@ -36,6 +36,7 @@ class _MyAppState extends ConsumerState<MyApp> {
         .getUserData(data.uid)
         .first;
     ref.read(userProvider.notifier).update((state) => userModel);
+    setState(() {});
   }
 
   @override
