4220db779bfe25bb2ff8f2af5883315a1931fb8a
iHamza7 <khanazam3324@gmail.com>
2023-06-16 20:23:13 +0500

    lib changed

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class _MyAppState extends ConsumerState<MyApp> {
     return ref.watch(authStateChangeProvider).when(
         data: (data) => MaterialApp.router(
               debugShowCheckedModeBanner: false,
-              title: 'Reddit Cline',
+              title: 'Reddit Clonne',
               // theme: ThemeData(
               theme: Pallete.darkModeAppTheme,
               routerDelegate: RoutemasterDelegate(
