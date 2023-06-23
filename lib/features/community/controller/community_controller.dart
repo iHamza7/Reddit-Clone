ead3f722a352bab1acdd9206064d1ea34f417a65
iHamza7 <khanazam3324@gmail.com>
2023-06-23 08:58:42 +0500

    lib changed

--- lib/features/community/controller/community_controller.dart
+++ lib/features/community/controller/community_controller.dart
@@ -8,6 +8,11 @@ import '../../auth/controllers/auth_controller.dart';
 import '../repository/community_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+final userCommunitiesProvider = StreamProvider((ref) {
+  final communityController = ref.watch(communityControllerProvider.notifier);
+  return communityController.getUserCommunities();
+});
+
 final communityControllerProvider =
     StateNotifierProvider<CommunityController, bool>((ref) {
   final communityRepository = ref.watch(communityRepositoryProvider);
