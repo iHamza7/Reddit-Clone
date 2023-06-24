de24030e1d5da1330834011cfd639a62134e5ec2
iHamza7 <khanazam3324@gmail.com>
2023-06-24 09:36:24 +0500

    lib changed

--- lib/features/community/screens/community_screen.dart
+++ lib/features/community/screens/community_screen.dart
@@ -1,12 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../../core/common/error.dart';
+import '../../../core/common/loader.dart';
+import '../controller/community_controller.dart';
+
 class CommunityScreen extends ConsumerWidget {
   final String name;
   const CommunityScreen({super.key, required this.name});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+    return Scaffold(
+      body: ref.watch(getCommunityByNameProvider(name)).when(
+            data: (community) => NestedScrollView(
+                headerSliverBuilder: (context, innerBoxIsScrolled) {
+                  return [
+                    SliverAppBar(
+                      expandedHeight: 150,
+                      flexibleSpace: Stack(
+                        children: [
+                          Positioned.fill(
+                            child: Image.network(community.banner),
+                          ),
+                        ],
+                      ),
+                    )
+                  ];
+                },
+                body: const Text('Displaying posts')),
+            error: (error, stackTrace) => ErrorText(text: error.toString()),
+            loading: () => const Loader(),
+          ),
+    );
   }
 }
