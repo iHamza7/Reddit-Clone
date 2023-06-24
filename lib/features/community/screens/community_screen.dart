f5ba80547ab8a3e4e87b79d95c4d5b461091882d
iHamza7 <khanazam3324@gmail.com>
2023-06-24 11:45:22 +0500

    lib changed

--- lib/features/community/screens/community_screen.dart
+++ lib/features/community/screens/community_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../core/common/error.dart';
 import '../../../core/common/loader.dart';
+import '../../auth/controllers/auth_controller.dart';
 import '../controller/community_controller.dart';
 
 class CommunityScreen extends ConsumerWidget {
@@ -11,6 +12,7 @@ class CommunityScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final user = ref.watch(userProvider)!;
     return Scaffold(
       body: ref.watch(getCommunityByNameProvider(name)).when(
             data: (community) => NestedScrollView(
@@ -30,7 +32,72 @@ class CommunityScreen extends ConsumerWidget {
                           ),
                         ],
                       ),
-                    )
+                    ),
+                    SliverPadding(
+                      padding: const EdgeInsets.all(16),
+                      sliver: SliverList(
+                        delegate: SliverChildListDelegate(
+                          [
+                            Align(
+                              alignment: Alignment.topLeft,
+                              child: CircleAvatar(
+                                backgroundImage: NetworkImage(community.avatar),
+                                radius: 35,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  'r/${community.name}',
+                                  style: const TextStyle(
+                                      fontSize: 19,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                community.mods.contains(user.uid)
+                                    ? OutlinedButton(
+                                        onPressed: () {},
+                                        style: ElevatedButton.styleFrom(
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(20),
+                                          ),
+                                          padding: const EdgeInsets.symmetric(
+                                            horizontal: 25,
+                                          ),
+                                        ),
+                                        child: const Text('Mod-Tools'),
+                                      )
+                                    : OutlinedButton(
+                                        onPressed: () {},
+                                        style: ElevatedButton.styleFrom(
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(20),
+                                          ),
+                                          padding: const EdgeInsets.symmetric(
+                                            horizontal: 25,
+                                          ),
+                                        ),
+                                        child: Text(
+                                            community.members.contains(user.uid)
+                                                ? 'Joined'
+                                                : 'Join'),
+                                      ),
+                              ],
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 10),
+                              child:
+                                  Text('r/${community.members.length} members'),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
                   ];
                 },
                 body: const Text('Displaying posts')),
