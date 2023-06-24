a6758e95c00df2b83e7040f8908f8a2aa8642604
iHamza7 <khanazam3324@gmail.com>
2023-06-24 09:15:41 +0500

    lib changed

--- lib/routes.dart
+++ lib/routes.dart
@@ -2,6 +2,7 @@ import 'package:routemaster/routemaster.dart';
 import 'package:flutter/material.dart';
 
 import 'features/auth/screens/login_screen.dart';
+import 'features/community/screens/community_screen.dart';
 import 'features/community/screens/create_community_screen.dart';
 import 'features/home/screens/home_screen.dart';
 
@@ -13,4 +14,8 @@ final loggedInRoute = RouteMap(routes: {
   '/': (_) => const MaterialPage(child: HomeScreen()),
   '/create-community': (_) =>
       const MaterialPage(child: CreateCommunityScreen()),
+  '/r/:name': (route) => MaterialPage(
+          child: CommunityScreen(
+        name: route.pathParameters['name']!,
+      )),
 });
