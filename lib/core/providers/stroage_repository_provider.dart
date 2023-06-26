54cd836cf8aa96e12d1aa9ca2a8fdbe86c87a594
iHamza7 <khanazam3324@gmail.com>
2023-06-26 20:39:03 +0500

    lib changed

--- lib/core/providers/stroage_repository_provider.dart
+++ lib/core/providers/stroage_repository_provider.dart
@@ -1,8 +1,29 @@
+import 'dart:io';
+
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:fpdart/fpdart.dart';
+
+import '../failure.dart';
+import '../type_def.dart';
 
 class StorageRepository {
   final FirebaseStorage _firebaseStorage;
 
   StorageRepository({required FirebaseStorage firebaseStorage})
       : _firebaseStorage = firebaseStorage;
+
+  FutureEither<String> storeFile({
+    required String path,
+    required String id,
+    required File? file,
+  }) async {
+    try {
+      final ref = _firebaseStorage.ref().child(path).child(id);
+      UploadTask uploadTask = ref.putFile(file!);
+      final snapshot = await uploadTask;
+      return right(await snapshot.ref.getDownloadURL());
+    } catch (e) {
+      return left(Failure(e.toString()));
+    }
+  }
 }
