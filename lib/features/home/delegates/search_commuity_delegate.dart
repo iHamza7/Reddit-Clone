ad507ed1c1e84a4d6a2c1420a41b060ad182f334
iHamza7 <khanazam3324@gmail.com>
2023-06-27 09:03:33 +0500

    lib changed

---
+++ lib/features/home/delegates/search_commuity_delegate.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class SearchCommunityDelegate extends SearchDelegate {
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
