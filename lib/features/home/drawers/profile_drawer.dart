b4ef70daca2c8907fe5d1cd7e7b4b47988042710
iHamza7 <khanazam3324@gmail.com>
2023-06-27 10:48:21 +0500

    lib changed

--- lib/features/home/drawers/profile_drawer.dart
+++ lib/features/home/drawers/profile_drawer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../../theme/pallete.dart';
 import '../../auth/controllers/auth_controller.dart';
 
 class ProfileDrawer extends ConsumerWidget {
@@ -11,30 +12,47 @@ class ProfileDrawer extends ConsumerWidget {
     final user = ref.watch(userProvider)!;
     return Drawer(
       child: SafeArea(
-          child: Column(
-        children: [
-          const SizedBox(
-            height: 5,
-          ),
-          CircleAvatar(
-            backgroundImage: NetworkImage(user.profilePic),
-            radius: 70,
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          Text(
-            'u/${user.name}',
-            style: const TextStyle(
-              fontSize: 15,
-              fontWeight: FontWeight.w500,
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 5,
             ),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-        ],
-      )),
+            CircleAvatar(
+              backgroundImage: NetworkImage(user.profilePic),
+              radius: 70,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              'u/${user.name}',
+              style: const TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Divider(),
+            ListTile(
+              onTap: () {},
+              title: const Text("My Profile"),
+              leading: const Icon(
+                Icons.person,
+              ),
+            ),
+            ListTile(
+              onTap: () {},
+              title: const Text("Log Out"),
+              leading: Icon(
+                Icons.logout,
+                color: Pallete.redColor,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
