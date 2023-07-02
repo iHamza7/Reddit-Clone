291374a630551b4d66a8554d7e32ad017ac589af
iHamza7 <khanazam3324@gmail.com>
2023-07-02 09:07:48 +0500

    lib changed

--- lib/features/community/screens/add_mods.dart
+++ lib/features/community/screens/add_mods.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class AddModsScreen extends ConsumerStatefulWidget {
-  const AddModsScreen({super.key});
+  final String name;
+  const AddModsScreen({super.key, required this.name});
 
   @override
   ConsumerState<ConsumerStatefulWidget> createState() => _AddModsScreenState();
@@ -11,6 +12,15 @@ class AddModsScreen extends ConsumerStatefulWidget {
 class _AddModsScreenState extends ConsumerState<AddModsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.save),
+          ),
+        ],
+      ),
+    );
   }
 }
