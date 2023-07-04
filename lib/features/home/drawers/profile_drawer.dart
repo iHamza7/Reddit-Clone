559e17419b7dd017792611368701b96ffe4d0e0c
iHamza7 <khanazam3324@gmail.com>
2023-07-04 20:45:09 +0500

    lib changed

--- lib/features/home/drawers/profile_drawer.dart
+++ lib/features/home/drawers/profile_drawer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:routemaster/routemaster.dart';
 
 import '../../../theme/pallete.dart';
 import '../../auth/controllers/auth_controller.dart';
@@ -9,6 +10,10 @@ class ProfileDrawer extends ConsumerWidget {
 
   void logOut(WidgetRef ref) {
     ref.read(authControllerProvider.notifier).logOut();
+  }
+
+  void navigateToUserProfile(BuildContext context, String uid) {
+    Routemaster.of(context).push('/u/$uid');
   }
 
   @override
@@ -40,7 +45,7 @@ class ProfileDrawer extends ConsumerWidget {
             ),
             const Divider(),
             ListTile(
-              onTap: () {},
+              onTap: () => navigateToUserProfile(context, user.uid),
               title: const Text("My profile"),
               leading: const Icon(
                 Icons.person,
--- lib/routes.dart
+++ lib/routes.dart
@@ -9,6 +9,7 @@ import 'features/community/screens/create_community_screen.dart';
 import 'features/community/screens/edit_community_screen.dart';
 import 'features/community/screens/mod_tools_screen.dart';
 import 'features/home/screens/home_screen.dart';
+import 'features/user_profile/screens/user_profile_screen.dart';
 
 final loggedOutRoute = RouteMap(routes: {
   '/': (_) => const MaterialPage(child: LoginScreen()),
@@ -34,5 +35,9 @@ final loggedInRoute = RouteMap(routes: {
   '/add-mods/:name': (routeData) => MaterialPage(
           child: AddModsScreen(
         name: routeData.pathParameters['name']!,
+      )),
+  '/u/:uid': (routeData) => MaterialPage(
+          child: UserProfileScreen(
+        uid: routeData.pathParameters['uid']!,
       )),
 });
