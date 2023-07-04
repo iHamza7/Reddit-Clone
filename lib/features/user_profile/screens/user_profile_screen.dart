8dd1637c436dbbebfb51b18a2d11c8390343c9de
iHamza7 <khanazam3324@gmail.com>
2023-07-04 21:04:02 +0500

    lib changed

--- lib/features/user_profile/screens/user_profile_screen.dart
+++ lib/features/user_profile/screens/user_profile_screen.dart
@@ -50,7 +50,7 @@ class UserProfileScreen extends ConsumerWidget {
                                   horizontal: 25,
                                 ),
                               ),
-                              child: const Text('edit prof'),
+                              child: const Text('edit profi'),
                             ),
                           ),
                         ],
