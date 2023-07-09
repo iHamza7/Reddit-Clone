5ab666f90786cb75365f99da16412f757dd83d47
iHamza7 <khanazam3324@gmail.com>
2023-07-09 10:57:53 +0500

    lib changed

--- lib/features/user_profile/screens/edit_profile_screen.dart
+++ lib/features/user_profile/screens/edit_profile_screen.dart
@@ -69,8 +69,10 @@ class _EditProfileScreenState extends ConsumerState<EditProfileScreen> {
   @override
   Widget build(BuildContext context) {
     final isLoading = ref.watch(userProfileControllerProvider);
+    final currentTheme = ref.watch(themeNotifierProvider);
     return ref.watch(getUserDataProvider(widget.uid)).when(
           data: (user) => Scaffold(
+            backgroundColor: currentTheme.colorScheme.background,
             appBar: AppBar(
               title: const Text('Edit Profile'),
               actions: [
