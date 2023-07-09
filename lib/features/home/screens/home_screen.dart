beaa1708f6d15c5b86942c4545ace36fccdaad2d
iHamza7 <khanazam3324@gmail.com>
2023-07-09 20:44:18 +0500

    lib changed

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../../theme/pallete.dart';
 import '../../auth/controllers/auth_controller.dart';
 import '../delegates/search_commuity_delegate.dart';
 import '../drawers/community_list_drawer.dart';
 import '../drawers/profile_drawer.dart';
 
-class HomeScreen extends ConsumerWidget {
+class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
 
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends ConsumerState<HomeScreen> {
+  int _page = 0;
   void openDrawer(BuildContext context) {
     Scaffold.of(context).openDrawer();
   }
@@ -17,9 +24,16 @@ class HomeScreen extends ConsumerWidget {
     Scaffold.of(context).openEndDrawer();
   }
 
+  void onPageChanged(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
+
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     final user = ref.watch(userProvider)!;
+    final currentTheme = ref.watch(themeNotifierProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Home"),
@@ -50,6 +64,20 @@ class HomeScreen extends ConsumerWidget {
       ),
       drawer: const CommunityListDrawer(),
       endDrawer: const ProfileDrawer(),
+      bottomNavigationBar: BottomNavigationBar(
+        fixedColor: currentTheme.iconTheme.color,
+        backgroundColor: currentTheme.colorScheme.background,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: '',
+          ),
+        ],
+      ),
     );
   }
 }
