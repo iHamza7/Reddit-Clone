4f91ac3d27e245f96b7965909aa36477aab7de84
iHamza7 <khanazam3324@gmail.com>
2023-07-11 20:53:22 +0500

    lib changed

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,7 @@ class _MyAppState extends ConsumerState<MyApp> {
     return ref.watch(authStateChangeProvider).when(
           data: (data) => MaterialApp.router(
             debugShowCheckedModeBanner: false,
-            title: 'Reddit Clone ap ',
+            title: 'Reddit Clone',
             // theme: ThemeData(
             theme: ref.watch(themeNotifierProvider),
             routerDelegate: RoutemasterDelegate(
