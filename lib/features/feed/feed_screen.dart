5376be7f9fa849bff167b2db67cd37e5647b4926
iHamza7 <khanazam3324@gmail.com>
2023-07-11 20:36:28 +0500

    lib changed

--- lib/features/feed/feed_screen.dart
+++ lib/features/feed/feed_screen.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../core/common/error.dart';
+import '../../core/common/loader.dart';
+import '../community/controller/community_controller.dart';
+import '../posts/controller/post_controller.dart';
+
 class FeedScreen extends ConsumerWidget {
   const FeedScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+    return ref.watch(userCommunitiesProvider).when(
+          data: (communities) => ref.watch(userPostProvider(communities)).when(
+                data: (data) {
+                  return ListView.builder(
+                      itemCount: data.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        final post = data[index];
+                        return;
+                      });
+                },
+                error: (error, stackTrace) => ErrorText(text: error.toString()),
+                loading: () => const Loader(),
+              ),
+          error: (error, stackTrace) => ErrorText(text: error.toString()),
+          loading: () => const Loader(),
+        );
   }
 }
