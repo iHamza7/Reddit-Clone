41d77ab10f48abc46f4f49042dcd48373d8d8dc1
iHamza7 <khanazam3324@gmail.com>
2023-07-11 09:48:10 +0500

    changed

--- lib/features/posts/controller/post_controller.dart
+++ lib/features/posts/controller/post_controller.dart
@@ -1,7 +1,12 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter/material.dart';
 
 import '../../../core/providers/stroage_repository_provider.dart';
+import '../../../models/community_models.dart';
+import '../../../models/post_model.dart';
+import '../../auth/controllers/auth_controller.dart';
 import '../repository/post_repository.dart';
+import 'package:uuid/uuid.dart';
 
 class PostController extends StateNotifier<bool> {
   final PostRepository _postRepository;
@@ -15,4 +20,30 @@ class PostController extends StateNotifier<bool> {
         _ref = ref,
         _storageRepository = storageRepository,
         super(false);
+
+  void shareTextPost({
+    required BuildContext context,
+    required String title,
+    required Community selectedCommnunity,
+    required String description,
+  }) async {
+    state = true;
+    String postId = const Uuid().v1();
+    final user = _ref.watch(userProvider)!;
+
+    final Post post = Post(
+        id: postId,
+        title: title,
+        communityName: selectedCommnunity.name,
+        communityProfilePic: selectedCommnunity.avatar,
+        upvotes: [],
+        downvotes: [],
+        commentCount: 0,
+        username: user.name,
+        uid: user.uid,
+        type: 'text',
+        createdAt: DateTime.now(),
+        awards: [],
+        description: description);
+  }
 }
