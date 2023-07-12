03a2b36c95356eff0d994910106feefaff6d76e5
iHamza7 <khanazam3324@gmail.com>
2023-07-12 21:06:40 +0500

    lib changed

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,7 @@ class _MyAppState extends ConsumerState<MyApp> {
     return ref.watch(authStateChangeProvider).when(
           data: (data) => MaterialApp.router(
             debugShowCheckedModeBanner: false,
-            title: 'Red',
+            title: 'Redd',
             // theme: ThemeData(
             theme: ref.watch(themeNotifierProvider),
             routerDelegate: RoutemasterDelegate(
