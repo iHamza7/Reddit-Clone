6b5c2d0a175f430af2bef5c342bd56e5bda537f5
iHamza7 <khanazam3324@gmail.com>
2023-08-18 08:54:58 +0500

    changed

--- lib/core/common/post_card.dart
+++ lib/core/common/post_card.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../models/post_model.dart';
+import '../../theme/pallete.dart';
 
 class PostCard extends ConsumerWidget {
   final Post post;
@@ -9,6 +10,28 @@ class PostCard extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+    final currentTheme = ref.watch(themeNotifierProvider);
+    final isTypeImage = post.type == 'image';
+    final isTypeText = post.type == 'text';
+    final isTypeLink = post.type == 'link';
+    return Column(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: currentTheme.drawerTheme.backgroundColor,
+          ),
+          padding: const EdgeInsets.symmetric(vertical: 10),
+          child: const Row(
+            children: [
+              Expanded(
+                child: Column(
+                  children: [],
+                ),
+              )
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
