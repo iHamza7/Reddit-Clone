3856a73868e9d1b6e9d7811f6ca651bbc66b5f91
iHamza7 <khanazam3324@gmail.com>
2023-08-18 10:19:14 +0500

    lib changed

--- lib/core/common/post_card.dart
+++ lib/core/common/post_card.dart
@@ -6,6 +6,8 @@ import '../../models/post_model.dart';
 import '../../theme/pallete.dart';
 import 'package:any_link_preview/any_link_preview.dart';
 
+import '../constants/constants.dart';
+
 class PostCard extends ConsumerWidget {
   final Post post;
   const PostCard({super.key, required this.post});
@@ -115,7 +117,35 @@ class PostCard extends ConsumerWidget {
                                 post.description!,
                                 style: const TextStyle(color: Colors.grey),
                               ),
-                            )
+                            ),
+                          Row(
+                            children: [
+                              Row(
+                                children: [
+                                  IconButton(
+                                      onPressed: () {},
+                                      icon: Icon(
+                                        Constants.up,
+                                        size: 30,
+                                        color: post.upvotes.contains(user.uid)
+                                            ? Pallete.redColor
+                                            : null,
+                                      )),
+                                  Text(
+                                      '${post.upvotes.length - post.downvotes.length == 0 ? 'Vote' : post.upvotes.length - post.downvotes.length}'),
+                                  IconButton(
+                                      onPressed: () {},
+                                      icon: Icon(
+                                        Constants.down,
+                                        size: 30,
+                                        color: post.downvotes.contains(user.uid)
+                                            ? Pallete.blueColor
+                                            : null,
+                                      ))
+                                ],
+                              )
+                            ],
+                          )
                         ],
                       ),
                     ),
