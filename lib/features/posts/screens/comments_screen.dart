507b3df5ce1e8b894bbb834d80991776680bb954
iHamza7 <khanazam3324@gmail.com>
2023-08-20 10:02:12 +0500

    lib changed

--- lib/features/posts/screens/comments_screen.dart
+++ lib/features/posts/screens/comments_screen.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../../core/common/error.dart';
+import '../../../core/common/loader.dart';
+import '../controller/post_controller.dart';
+
 class CommentsSceen extends ConsumerStatefulWidget {
-  const CommentsSceen({super.key});
+  final String postId;
+  const CommentsSceen({super.key, required this.postId});
 
   @override
   ConsumerState<ConsumerStatefulWidget> createState() => _CommentsSceenState();
@@ -12,7 +17,21 @@ class _CommentsSceenState extends ConsumerState<CommentsSceen> {
   final commentsController = TextEditingController();
 
   @override
+  void dispose() {
+    super.dispose();
+    commentsController.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: ref.watch(getUserByIdProvider(widget.postId)).when(
+            data: (data) {
+              return null;
+            },
+            error: (error, stackTrace) => ErrorText(text: error.toString()),
+            loading: () => const Loader(),
+          ),
+    );
   }
 }
