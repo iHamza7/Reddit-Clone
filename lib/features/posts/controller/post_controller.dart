4c99e602d573fafd0d0c2522662b938533756e6e
iHamza7 <khanazam3324@gmail.com>
2023-08-22 20:12:03 +0500

    lib changed

--- lib/features/posts/controller/post_controller.dart
+++ lib/features/posts/controller/post_controller.dart
@@ -182,7 +182,7 @@ class PostController extends StateNotifier<bool> {
         .read(userProfileControllerProvider.notifier)
         .updateUserKarma(UserKarma.deletePost);
     res.fold(
-        (l) => null, (r) => showSnackBar(context, 'Post Del Successfully'));
+        (l) => null, (r) => showSnackBar(context, 'Post Dele Successfully'));
   }
 
   void upvote(Post post) async {
