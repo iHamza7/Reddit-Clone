2282b16ecb05c729d6b3a953ced20f82aa07aa97
iHamza7 <khanazam3324@gmail.com>
2023-08-23 20:21:58 +0500

    lib changed

--- lib/core/common/signin_button.dart
+++ lib/core/common/signin_button.dart
@@ -6,10 +6,13 @@ import '../../theme/pallete.dart';
 import '../constants/constants.dart';
 
 class SignInButton extends ConsumerWidget {
-  const SignInButton({super.key});
+  final bool isFromLogin;
+  const SignInButton({super.key, this.isFromLogin = true});
 
   void signInWithGoogle(BuildContext context, WidgetRef ref) {
-    ref.read(authControllerProvider.notifier).signInWithGoogle(context);
+    ref
+        .read(authControllerProvider.notifier)
+        .signInWithGoogle(context, isFromLogin);
   }
 
   @override
