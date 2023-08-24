8f5f6dd322fc30835ea91e0bfbca82f94af06fa6
iHamza7 <khanazam3324@gmail.com>
2023-08-24 20:21:52 +0500

    lib changed

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -38,7 +38,7 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
     final currentTheme = ref.watch(themeNotifierProvider);
     return Scaffold(
       appBar: AppBar(
-        title: const Text(""),
+        title: const Text("H"),
         centerTitle: false,
         leading: Builder(builder: (context) {
           return IconButton(
