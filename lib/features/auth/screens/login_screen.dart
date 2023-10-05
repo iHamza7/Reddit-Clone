369d56649475ae46573ecda43ce09a20c18c52cb
iHamza7 <khanazam3324@gmail.com>
2023-10-05 11:50:25 +0500

    changed

--- lib/features/auth/screens/login_screen.dart
+++ lib/features/auth/screens/login_screen.dart
@@ -16,57 +16,61 @@ class LoginScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final isLoading = ref.watch(authControllerProvider);
-    return Scaffold(
-      appBar: AppBar(
-        title: Image.asset(
-          Constants.logoPath,
-          height: 40,
-        ),
-        centerTitle: true,
-        actions: [
-          TextButton(
-            onPressed: () => signInAsGuest(ref, context),
-            child: const Text(
-              'Guest ',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 16,
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Image.asset(
+            Constants.logoPath,
+            height: 40,
+          ),
+          centerTitle: true,
+          actions: [
+            TextButton(
+              onPressed: () => signInAsGuest(ref, context),
+              child: const Text(
+                'Guest ',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
+        body: SafeArea(
+          child: isLoading
+              ? const Loader()
+              : Column(
+                  children: [
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    const Text(
+                      'Dive into anything',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 0.5,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Image.asset(
+                        Constants.loginEmotePath,
+                        height: 400,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const SignInButton(),
+                  ],
+                ),
+        ),
       ),
-      body: isLoading
-          ? const Loader()
-          : Column(
-              children: [
-                const SizedBox(
-                  height: 30,
-                ),
-                const Text(
-                  'Dive into anything',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 0.5,
-                  ),
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Image.asset(
-                    Constants.loginEmotePath,
-                    height: 400,
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                const SignInButton(),
-              ],
-            ),
     );
   }
 }
